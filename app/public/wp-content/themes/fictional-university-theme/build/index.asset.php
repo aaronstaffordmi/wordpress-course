a50e02f3f66a83f01a7763a5b2c3caf6bfc61630
Aaron Stafford <aaronstafford.co@gmail.com>
2022-10-28 20:30:18 -0400

    whatever

---
+++ app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'd7defb0f24664b4efdaa');
--- app/public/wp-content/themes/fictional-university-theme/header.php
+++ app/public/wp-content/themes/fictional-university-theme/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <?php wp_head(); ?>
-
+    
     <title>Document</title>
 </head>
 <body>
